84588f2da0ee59db9bb6339b64205d7ca469590a
Weiyu Hao <whao5@circinus-12.ics.uci.edu>
2023-04-23 15:50:41 -0700

    new comments

--- scraper.py
+++ scraper.py
@@ -16,6 +16,16 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+    
+    # Correctness:
+    # Honor the politeness delay for each site - 0.5s
+    # Crawl all pages with high textual information content
+    # Detect and avoid infinite traps
+    # Detect and avoid sets of similar pages with no information
+    # Detect redirects and if the page redirects your crawler, index the redirected content
+    # Detect and avoid dead URLs that return a 200 status but no data (click here to see what the different HTTP status codes mean Links to an external site.)
+    # Detect and avoid crawling very large files, especially if they have low information value
+    
     soup = BeautifulSoup(resp.raw_response, "html.parser")
     links = []
     for link in soup.find_all('a'):
