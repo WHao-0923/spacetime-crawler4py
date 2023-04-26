c189cb5fd3cdcf3d671ec4b5169250487a4a3219
Weiyu Hao <whao5@odin.ics.uci.edu>
2023-04-25 17:51:55 -0700

    Working scraper with Added validity

--- scraper.py
+++ scraper.py
@@ -1,6 +1,11 @@
 import re
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup 
+import time
+
+MIN_CONTENT_LENGTH = 500
+MAX_CONTENT_LENGTH = 100000
+crawled = set()
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -19,28 +24,71 @@ def extract_next_links(url, resp):
     
     # Correctness:
     # Honor the politeness delay for each site - 0.5s
-    # Crawl all pages with high textual information content
-    # Detect and avoid infinite traps
-    # Detect and avoid sets of similar pages with no information
-    # Detect redirects and if the page redirects your crawler, index the redirected content
-    # Detect and avoid dead URLs that return a 200 status but no data (click here to see what the different HTTP status codes mean Links to an external site.)
-    # Detect and avoid crawling very large files, especially if they have low information value
-    
+    # Crawl all pages with high textual information content - #
+    # Detect and avoid infinite traps - #
+    # Detect and avoid sets of similar pages with no information - /#
+    # Detect redirects and if the page redirects your crawler, index the redirected content - /#
+    # Detect and avoid dead URLs that return a 200 status but no data - /#
+    # Detect and avoid crawling very large files, especially if they have low information value -/#
+    # Make sure to add relative links to current and return - #
+    # time.sleep(0.5) // Already use politeness in config
+
+    # if (resp.status >= 400 or resp.status == 204):
+    #     
+    #     return list()
+
+    # Check if the response status is 200 (OK)
+    if resp.status != 200:
+        print(1)
+        return []
+
+    # Check the content length before processing the URL
+    if len(resp.raw_response.content) < MIN_CONTENT_LENGTH:
+        print(2)
+        return []
+
+    # Check the content length before processing the URL
+    if len(resp.raw_response.content) > MAX_CONTENT_LENGTH:
+        print(3)
+        return []
+
+    # check duplicates/infinite loop
+    dup = set(url)
+
     soup = BeautifulSoup(resp.raw_response.content, "html.parser")
     links = []
     for link in soup.find_all('a'):
         href = link.get('href')
-        #print(is_valid(href))
-        #print("ori: ",href)
-        if href and is_valid(href):
-            parsed_href = urlparse(href)
-            #print("parsed: ",parsed_href)
-            # Check if the URL is valid
-            #print(parsed_href.scheme, parsed_href.netloc)
-            ori_parsed = urlparse(url)
-            if parsed_href.scheme and parsed_href.netloc == ori_parsed.netloc:
+
+        #links.append(href)
+
+        # check relative path
+        if href.startswith('.') or href.startswith('/'):
+            abs_url = url + href
+            # delete fragments
+            if "#" in abs_url:
+                abs_url = abs_url.split('#')[0]
+            # check for duplicates
+            if abs_url not in dup and href not in crawled:
+                links.append(abs_url)
+                dup.add(abs_url)
+                crawled.add(abs_url)
+            continue
+        parsed_href = urlparse(href)
+        # Check if the URL is valid
+        ori_parsed = urlparse(url)
+        if parsed_href.scheme and parsed_href.netloc == ori_parsed.netloc:
+            # check for duplicates
+            if href not in dup and href not in crawled:
+                # delete fragments
+                if "#" in href:
+                    href = href.split('#')[0]
                 links.append(href)
-    #print(links)
+                dup.add(href)
+                crawled.add(href)
+    # print(links)
+    # print(len(links))
+    # exit()
     return links
 
 def is_valid(url):
@@ -50,6 +98,8 @@ def is_valid(url):
     try:
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
+            return False
+        if "redirect" in url:
             return False
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
